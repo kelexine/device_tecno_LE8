394ca016aa074d84a99a6eae4fd6395c7325cde2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-01-03 07:39:14 +0100

    TECNO-LE8: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO LE8 Script version: 3.0.0

---
+++ omni_TECNO-LE8.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-LE8 device
+$(call inherit-product, device/tecno/TECNO-LE8/device.mk)
+
+PRODUCT_DEVICE := TECNO-LE8
+PRODUCT_NAME := omni_TECNO-LE8
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO LE8
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_le8_h771-user 11 RP1A.200720.011 78515 release-keys"
+
+BUILD_FINGERPRINT := TECNO/LE8-GL/TECNO-LE8:11/RP1A.200720.011/220902V932:user/release-keys
